fe7c80a12491cf73a3ed44a419d6302afcbd8a4c
Hugo Leclerc <hugal.leclerc@gmail.com>
2017-05-05 15:19:02 +0200

    Codegen in != directories

--- src/GeneratorCodegen.ts
+++ src/GeneratorCodegen.ts
@@ -14,8 +14,9 @@ class GeneratorCodegen extends Generator {
         funcs.push( { prio: 0, func: ( target: string, output: string, cwd: string, cb: ( cn: CompilationNode ) => void, for_found, care_about_target: boolean, allow_generation: boolean ): void => {
             if ( allow_generation ) {
                 const filename = path.basename( target );
+                const n_cwd = path.resolve( cwd, path.dirname( target ) );
                 if ( filename[ 0 ] == '(' && filename.lastIndexOf( ')' ) > 0 )
-                    return cb( this.env.New( "Codegen", [], { output, filename, cwd } ) );
+                    return cb( this.env.New( "Codegen", [], { output, filename, cwd: n_cwd } ) );
             }
             return cb( null );
         } } );
